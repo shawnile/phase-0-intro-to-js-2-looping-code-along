3fc2edbafa563ffbb1788f20f253a15f3b31568d
shawnil <ervin.shawnil@gmail.com>
2021-12-06 15:41:25 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, birthday) {
+    let thankYouCards = []
+    for ( let i = 0; i < names.length; i++ ) {
+      thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${birthday} gift!` )
+    }
+    return thankYouCards;
+  }
+
+  function countDown(startingNumber) {
+    while (startingNumber > 0 ) {
+      console.log(startingNumber);
+      startingNumber -= 1;
+    }
+    console.log(startingNumber);
+  }
